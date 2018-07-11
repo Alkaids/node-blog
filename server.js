0b2558332f3639da74ae1af014fce5d2f3b6f5dc
kingduidui <m18508218948@163.com>
2018-07-11 21:16:53 +0800

    加入静态页面和数据库连接

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
-const express = requier('express');
-const expressStatic = requier('express-static');
-const bodyParser = requier('body-parser');
-const cookieParser = requier('cookie-parser');
-const cookieSession = requier('cookie-session');
-const multer = requier('multer');
-const consolidate = requier('consolidate');
-const fs = require('fs');
+const express = require('express');
+const expressStatic = require('express-static');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+const multer = require('multer');
+const consolidate = require('consolidate');
+const mysql = require('mysql');
 
-// 实例
-let server = express();
-// 防session劫持keys数组
-let keys = new Array(100000).map(() => {
-    return 'key_' + Math.random();
+// express实例
+const server = express();
+
+// mysql 连接池
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'jinxin2828',
+    database: 'blog'
 });
 
+// 防session劫持keys数组
+const keys = [];
+for (let i = 0; i < 100000; i++) {
+    keys.push('key_' + Math.random());
+}
+
 // 引擎配置
-server.set('view engine','html');
-server.set('views',__dirname + '/view');
-server.engine('html',consolidate.ejs);
+server.set('view engine', 'html');
+server.set('views', __dirname + '/view');
+server.engine('html', consolidate.ejs);
 
 // post解析
-server.use(bodyParser({ extended: false }));
+server.use(bodyParser.urlencoded({ extended: false }));
 server.use(multer({ dest: __dirname + '/dest' }).any());
 
 // cookie session
@@ -32,10 +42,18 @@ server.use(cookieSession({
 }));
 
 // 用户请求
-server.use('/', (req, res) => {
-    if (req.path != './favicon') { // 过滤掉chrome的favicon
-        console.log(req.body, req.cookies, req.session);
-    }
+server.get('/', (req, res) => {
+
+    let insert = 'SELECT * FROM `banner_table`'
+
+    db.query(insert, (err, data) => {
+        if (err) {
+            res.status(500).send('database error' + err).end();
+        } else {
+            res.render('index.ejs', { banner: data });
+        }
+    });
+
 });
 
 // 静态文件
